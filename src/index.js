2da46f7db6f248209831b3e25deaad9025761a70
elihe999 <eli.he999@hotmail.com>
2019-07-25 17:31:24 +0800

    <chore>React basic element

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import React from 'react';
-import { render } from 'react-dom';
+// import { render } from 'react-dom';      //1
+import ReactDOM from 'react-dom'
 
-const root = document.querySelector('#root');
-render(<div>Hello world</div>, root);
+const user = {firstName: 'tom', lastName: 'jerry'}
+const greet = <p>hello everyone.</p>
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName
+}
+
+const arr = [1,2,3].map(num => <li key={num}>{num}</li>)
+
+const jsx = (
+    <div>
+        <h1>React Nice!</h1>
+        {/** */}
+        <p>{formatName(user)}</p>
+        {greet}
+        <ul>{arr}</ul>
+    </div>
+);
+
+console.log(jsx)
+ReactDOM.render(jsx, document.getElementById('root'))
+// const root = document.querySelector('#root');        //1
+// render(<div>Hello world</div>, root);    //1
