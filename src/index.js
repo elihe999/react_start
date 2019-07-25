e90c07785940a7e3099323283e7f4ac953d49110
elihe999 <eli.he999@hotmail.com>
2019-07-25 17:56:39 +0800

    <chore>Using css in react

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 // import { render } from 'react-dom';      //1
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import logo from './logo.svg';
+import style from "./index.module.css";
+// import "./index.css";
 
 const user = {firstName: 'tom', lastName: 'jerry'}
 const greet = <p>hello everyone.</p>
@@ -9,14 +12,17 @@ function formatName(user) {
 }
 
 const arr = [1,2,3].map(num => <li key={num}>{num}</li>)
-
+const name = <h1>Default Name</h1>
 const jsx = (
     <div>
-        <h1>React Nice!</h1>
+        {name ? <h1>React Nice!</h1> : null}
         {/** */}
         <p>{formatName(user)}</p>
         {greet}
         <ul>{arr}</ul>
+        {/** The `style` prop expects a mapping from style properties to values, not a string.  */}
+        {/* <img src={logo} alt="logo" style="width:100px" /> */}
+        <img src={logo} alt="logo" style={{width: 100}} className={style.img} />
     </div>
 );
 
