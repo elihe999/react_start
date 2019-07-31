308830ef81f9ad7566d07a578735d7b9e4031a0e
yili <eli.he999@hotmail.com>
2019-07-31 20:35:50 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,22 @@
+import React, {Component} from "react";
+// import JsxTest from "./components/JsxTest.js"
+import StateMgt from "./components/StateMgt.js"
 
+// function App() {
+//     return (
+//         <div>
+//             <JsxTest />
+//         </div>
+//     )
+// }
 
+class App extends Component {
+    render () {
+        return (
+            <div>
+                <StateMgt />
+            </div>
+        )
+    }
+}
 export default App;
