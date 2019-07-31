712b75d36adffd6f9b166b39546e9c3fd5356621
yili <eli.he999@hotmail.com>
2019-07-31 20:35:27 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,3 @@
-import React from "react";
-import JsxTest from "./components/JsxTest.js"
 
-function App() {
-    return (
-        <div>
-            <JsxTest />
-        </div>
-    )
-}
 
 export default App;
